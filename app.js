3414b574cf8be1056f69f999d3e8d6d588e40c2b
Shdmn-k <shadmeenbk@gmail.com>
2024-12-26 16:51:30 +0530

    module 2 mock 2

---
+++ app.js
@@ -0,0 +1,34 @@
+const questions =[
+    {
+        question: "Which of the following methods is used to update?",
+         option : ["GET","POST","PATCH","DELETE",],
+        "correctAnswer": "PATCH",
+      },
+      {
+        question: "Which of the following methods is used to update?",
+         option : ["GET","POST","PATCH","DELETE",],
+        "correctAnswer": "PATCH",
+      },
+      {
+        question: "Which of the following methods is used to update?",
+         option : ["GET","POST","PATCH","DELETE",],
+        "correctAnswer": "PATCH",
+      },
+      {
+        question: "Which of the following methods is used to update?",
+         option : ["GET","POST","PATCH","DELETE",],
+        "correctAnswer": "PATCH",
+      },
+      
+];
+let currentQuestionIndex=0;
+let score =0;
+ const loadQuestions = () =>{
+    const currentQuestion = questions[currentQuestionIndex];
+    const questionContainer = document.getElementById("question-container")
+    const questionIndex = document.getElementById("question-index")
+    // if (currentQuestion){
+    //     questionContainer.innerHTML=
+        
+    // }
+ }
